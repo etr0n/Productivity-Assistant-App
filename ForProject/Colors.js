7eb2363c05c34dc42728cf9db413b690d0df537a
Vigaile <61386951+Vigaile@users.noreply.github.com>
2020-04-29 19:47:11 +0300

    Add files via upload

---
+++ ForProject/Colors.js
@@ -0,0 +1,10 @@
+export default colors = {
+    black: "#000000",
+    pink: "#A3213F",
+    lightPink: "#D9B0CB",
+    white: "#FFFFFF",
+    greyblue: "#2F384B",
+    darkgreyblue: "#0a1824",
+    grey: "#49556E",
+    lightGrey: "#BFBFBF"
+};
